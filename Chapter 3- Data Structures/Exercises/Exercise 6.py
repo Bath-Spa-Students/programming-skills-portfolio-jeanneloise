efdb71589a555a1b0c4eb4e4e4ccf553f0aedd4a
jeanneloise <togononjeanne11@gmail.com>
2023-10-23 09:38:08 +0400

    Exercise 6.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 6.py
@@ -0,0 +1,31 @@
+attn= "Attention guests, due to some inconvenience I can only inivite two guests"
+print (attn)
+
+print("\n")
+
+guests=['Josh', 'Drake', 'Miranda', 'Paul', 'Dalton']
+print (guests)
+
+print("\n")
+
+no_invite= f"Good day {guests[2]}, unfortunately we no longer have space for you."
+print (no_invite)
+no_invite= f"Good day {guests[3]}, unfortunately we no longer have space for you."
+print (no_invite)
+no_invite= f"Good day {guests[4]}, unfortunately we no longer have space for you."
+print (no_invite)
+
+
+
+print("\n")
+
+invt_msg= f"Greetings {guests[0]}, you are invited to dinner."
+print (invt_msg)
+invt_msg= f"Greetings {guests[1]}, you are invited to diinner."
+print (invt_msg)
+
+
+
+
+
+
