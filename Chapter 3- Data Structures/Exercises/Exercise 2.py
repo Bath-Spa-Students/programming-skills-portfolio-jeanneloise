1ee96103912fae4ee5c9b6e70ba6f45f27c5aa3b
jeanneloise <togononjeanne11@gmail.com>
2023-10-23 09:36:59 +0400

    Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,11 @@
+
+friends= ['Zoe','Freya','Hannah']
+
+message=f"Hello,{friends[0]}."
+print (message)
+
+message=f"Hello,{friends[1]}."
+print (message)
+
+message=f"Hello,{friends[2]}."
+print (message)
