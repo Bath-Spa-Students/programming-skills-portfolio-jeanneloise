4414b2fa57bb60b6dde3e921d51a4240223612d3
jeanneloise <togononjeanne11@gmail.com>
2023-10-23 09:37:12 +0400

    Exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 4.py
@@ -0,0 +1,13 @@
+dinner= ['Rudy', 'Boy', 'Nedi']
+print (dinner)
+
+
+
+guests= f'{dinner[0]} you are invited to my dinner party.'
+print (guests)
+
+guests= f'{dinner[1]} you are invited to my dinner party.'
+print (guests)
+
+guests= f'{dinner[2]} you are invited to my dinner party.'
+print (guests)
