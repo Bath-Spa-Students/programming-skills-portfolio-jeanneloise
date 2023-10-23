fcc4b9aabb8b336ee3a355e66438495736441d0c
jeanneloise <togononjeanne11@gmail.com>
2023-10-23 09:37:30 +0400

    Exercise 5.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 5.py
@@ -0,0 +1,24 @@
+guests= ["Zoe", "Freya", "Hannah", "Avila"]
+print (guests)
+
+name=guests[0]
+print(f"\n{name} can't make it to dinner")
+
+print ("\n")
+
+new_guests= ["Zoe", "Freya", "Hannah", "Avila"]
+new_guests[0]= "Dowe"
+print (new_guests[0:4])
+
+coming= f"\n{new_guests[0]}, please come to dinner."
+print (coming)
+
+coming= f"{new_guests[1]}, please come to dinner."
+print (coming)
+
+coming= f"{new_guests[2]}, please come to dinner."
+print (coming)
+
+coming= f"{new_guests[3]}, please come to dinner."
+print (coming)
+
