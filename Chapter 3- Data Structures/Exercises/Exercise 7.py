7de98cc22cec2113e5f9cbfbad433d5cc91af1d5
jeanneloise <togononjeanne11@gmail.com>
2023-10-23 09:38:18 +0400

    Exercise 7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 7.py
@@ -0,0 +1,16 @@
+world=['England', 'Korea', 'Maldives', 'Japan', 'Greece']
+
+print("original order:")
+print (world)
+
+print("\nAlphabetical:")
+print(sorted(world))
+
+print("\nReverse Alphabetical:")
+print(sorted(world,reverse=True))
+
+print("\nReversed:")
+world.reverse()
+print(world)
+
+
