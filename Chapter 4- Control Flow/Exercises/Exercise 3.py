f4283e910c029b4ced9e57b237e7e3d94bbe71a8
jeanneloise <togononjeanne11@gmail.com>
2023-10-30 18:39:31 +0400

    Exercise 3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 3.py
@@ -0,0 +1,14 @@
+alien_color= "green"
+if alien_color =="red":
+    print ("You just earned 5 points")
+
+
+
+elif alien_color =="yellow":
+    print ("You just earned 10 points")
+
+else:
+    print ("You just earned 15 points")
+
+
+
