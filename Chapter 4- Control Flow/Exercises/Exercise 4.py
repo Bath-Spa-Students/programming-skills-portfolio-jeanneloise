4a6dd33ad1f0a6adc3b5049d855e4ef5ef72b35f
jeanneloise <togononjeanne11@gmail.com>
2023-10-30 18:40:18 +0400

    Exercise 4.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 4.py
@@ -0,0 +1,24 @@
+person=33456
+
+if person <2:
+    print("Baby")
+
+elif person<4:
+    print("Todler")
+
+elif person<13:
+    print("Kid")
+
+elif person<20:
+    print("Teenager")
+
+elif person<65:
+    print("Adult")
+
+else:
+    print("Elder")
+
+
+
+
+
