7fe8400a19a46d78e81d59e79f2e85d97a3e2e76
jeanneloise <togononjeanne11@gmail.com>
2023-11-02 19:12:03 +0400

    Exercise 3.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 3.py
@@ -0,0 +1,14 @@
+glossary = { 'variable': 'A variable is a named container for a particular set of bits or type of data. ',
+            'value': 'The representation of some entity that can be manipulated by a program.',
+            'if': 'A programming conditional statement that, if proved true, performs a function or displays information.',
+            'print': 'A function that sends text, variables, or another object to the screen.',
+            'dictionary': 'A general-purpose data structure for storing a group of objects.',
+            'list': 'A list or sequence is an abstract data type that represents a finite number of ordered values, where the same value may occur more than once.',
+            'string': 'A string is traditionally a sequence of characters, either as a literal constant or as some kind of variable. ',
+            'float': 'A float is a data type composed of a number that is not an integer, because it includes a fraction represented in decimal format.',
+            'key': 'A key in the context of data structures is a value that is used to identify a certain component of a data-structure.',
+            'strip': 'A technique used for scheduling television and radio programming to ensure consistency and coherency.'}
+
+
+for word, definition in glossary.items():
+    print(f"\n{word}: {definition}")
