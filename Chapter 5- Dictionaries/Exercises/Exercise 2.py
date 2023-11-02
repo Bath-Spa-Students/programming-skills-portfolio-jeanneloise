5ee49fcdffcd08196032aeffecff4f12033125c9
jeanneloise <togononjeanne11@gmail.com>
2023-11-02 19:11:44 +0400

    Exercise 2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 2.py
@@ -0,0 +1,11 @@
+glossary = { 'variable': 'A variable is a named container for a particular set of bits or type of data. ',
+            'value': 'The representation of some entity that can be manipulated by a program.',
+            'if': 'A programming conditional statement that, if proved true, performs a function or displays information.',
+            'print': 'A function that sends text, variables, or another object to the screen.',
+            'dictionary': 'A general-purpose data structure for storing a group of objects.'}
+
+print (f"\n{'variable'}: {glossary['variable']}")
+print (f"\n{'value'}: {glossary['value']}")
+print (f"\n{'if'}: {glossary['if']}")
+print (f"\n{'print'}: {glossary['print']}")
+print (f"\n{'dictionary'}: {glossary['dictionary']}")
