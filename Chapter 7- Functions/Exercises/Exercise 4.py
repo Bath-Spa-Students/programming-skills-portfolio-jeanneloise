a9dda51e2b8fe3b30135f903d76ea02d430c3c69
jeanneloise <togononjeanne11@gmail.com>
2023-11-14 10:42:13 +0400

    Exercise 4.py

---
+++ Chapter 7- Functions/Exercises/Exercise 4.py
@@ -0,0 +1,8 @@
+def make_shirt(size='large', text = 'i love python'):
+  
+    print(f"\nI am making my t-shirt {size} sized.")
+    print(f'The message will say, "{text}"')
+
+make_shirt()
+make_shirt(size='small')
+make_shirt(text="i love cobra.", size='medium')
