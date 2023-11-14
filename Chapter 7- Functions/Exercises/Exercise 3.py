1b50192823fe32dd619c4ab567f91e3f86629ba8
jeanneloise <togononjeanne11@gmail.com>
2023-11-14 10:42:05 +0400

    Exercise 3.py

---
+++ Chapter 7- Functions/Exercises/Exercise 3.py
@@ -0,0 +1,10 @@
+
+def make_shirt(size, text):
+  
+    print(f"\nI am making my t-shirt {size} sized.")
+    print(f'The message will say, "{text}"')
+
+make_shirt('small', 'Dowe')
+make_shirt(text="Dimsum lover.", size='large')
+    
+    
